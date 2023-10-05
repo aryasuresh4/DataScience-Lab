442893cad57069230409d397c36d7ecfb0e8ca12
anjukrishna07 <117815090+anjukrishna07@users.noreply.github.com>
2023-10-05 13:40:27 +0530

    Create number.py

---
+++ number.py
@@ -0,0 +1,27 @@
+print("Demo of basic data types: Number")
+x=3
+y=2.5
+print("x=",x)
+print("y=",y)
+print("data type of variable x:",type(x))
+print("data type of variable y:",type(y))
+print("Addition:",x+y)
+print("Substraction:",x-y)
+print("multiplication:",x*2)
+print("Exponential:",x**2)
+
+
+
+
+output:
+Demo of basic data types: Number
+x= 3
+y= 2.5
+data type of variable x: <class 'int'>
+data type of variable y: <class 'float'>
+Addition: 5.5
+Substraction: 0.5
+multiplication: 6
+Exponential: 9
+
+
